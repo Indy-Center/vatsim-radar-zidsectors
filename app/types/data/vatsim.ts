db7a34625ba9431db02a17892c7b9ba63aa16588
daniluk4000 <daniluk4000@outlook.com>
2026-04-16 23:39:28 +0300

    various vIFF integration updates

--- app/types/data/vatsim.ts
+++ app/types/data/vatsim.ts
@@ -439,6 +439,9 @@ export enum ViffStatus {
     SLC = 'SLC',
     // Flight is already in movement. Automatically set when AOBT is set.
     ATC_ACTIV = 'ATC_ACTIV',
+    REA = 'REA',
+    COMPLY = 'COMPLY',
+    AIRB = 'AIRB',
 }
 
 export interface IpfsUser {
