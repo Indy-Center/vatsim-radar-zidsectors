6338653bede108bba8ede035a69343e74625f472
daniluk4000 <daniluk4000@outlook.com>
2026-04-20 21:24:10 +0300

    Final IPFS fixes

--- app/components/map/overlays/pilot/MapPilotOverlay.vue
+++ app/components/map/overlays/pilot/MapPilotOverlay.vue
@@ -437,6 +437,7 @@ const sections = computed<InfoPopupSection[]>(() => {
             collapsedDefault: !!collapsedViff.value,
             collapsedDefaultOnce: true,
             collapsible: true,
+            bubble: props.overlay?.data.ipfs.isCdm ? 'CDM online' : undefined,
         });
     }
 
